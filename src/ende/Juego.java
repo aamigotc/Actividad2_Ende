c4c8d9590664bb4a91f58974ec2f8b490bfed38b
aamigotc <aamigotc@gmail.com>
2022-05-16 17:27:26 +0200

    variables declaradas antes del codigo

--- src/ende/Juego.java
+++ src/ende/Juego.java
@@ -9,12 +9,12 @@ public class Juego
 {
     public static void main(String args[])
     {
-        Jugador p1=new Jugador();
-        Jugador p2=new Jugador();
-        boolean fin_de_juego=false;  
+        Jugador p1 = new Jugador();
+        Jugador p2 = new Jugador();
+        boolean fin_de_juego = false;
         Integer Rondasjugadas = 0;    // Número de rondas jugadas
-        Integer EXITOS_jugador1=p1.éxitos;
-        Integer EXITOS_jugador2=p2.éxitos;
+        Integer EXITOS_jugador1 = p1.éxitos;
+        Integer EXITOS_jugador2 = p2.éxitos;
         Integer EmPaTeS = 0;
         String opcion_JUGADOR1;
         String opcion_JUGADOR2;
@@ -22,11 +22,14 @@ public class Juego
         // Bucle de juego
         do
         {
+            opcion_JUGADOR1=p1.opcion_al_azar();
+            opcion_JUGADOR2 = p2.opcion_al_azar();
+
             System.out.println("***** Ronda: " + Rondasjugadas+" *********************\n");
             System.out.println("Numero de empates: "+ EmPaTeS + "\n");
-            opcion_JUGADOR1=p1.opcion_al_azar();
+
             System.out.println("Jugador 1: " + opcion_JUGADOR1+"\t Jugador 1 - Partidas ganadas: " + EXITOS_jugador1);
-            opcion_JUGADOR2 = p2.opcion_al_azar();
+
             System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
             
             if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel")))
@@ -87,6 +90,7 @@ class Jugador{
     {
         String opcion="";
         Integer c = (int)(Math.random()*3);
+
         switch(c){
             case 0:
             	opcion=("piedra");
